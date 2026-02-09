dcde3a1d6fbdfbafdc61c09bf224188eabbf662e
george-sx <stewgeorge3@gmail.com>
2026-02-09 17:06:39 -0500

    Mark SessionTable methods inline(always)

--- crates/moldudp64-core/src/sessions.rs
+++ crates/moldudp64-core/src/sessions.rs
@@ -3,12 +3,14 @@ use crate::types::{SequenceNumber, SessionID};
 use std::collections::HashMap;
 
 impl SessionTable {
+    #[inline(always)]
     pub fn new() -> Self {
         SessionTable {
             sessions: HashMap::new(),
         }
     }
 
+    #[inline(always)]
     pub fn generate_session_id(&mut self) -> SessionID {
         let s = format!("MM{:08}", self.sessions.len() + 1);
         let mut session_id = [b' '; 10];
@@ -17,14 +19,17 @@ impl SessionTable {
         session_id
     }
 
+    #[inline(always)]
     pub fn add_session(&mut self, session_id: SessionID, sequence_number: SequenceNumber) {
         self.sessions.insert(session_id, sequence_number);
     }
 
+    #[inline(always)]
     pub fn get_sequence_number(&self, session_id: SessionID) -> Option<&SequenceNumber> {
         self.sessions.get(&session_id)
     }
 
+    #[inline(always)]
     pub fn remove_session(&mut self, session_id: &SessionID) {
         self.sessions.remove(session_id);
     }
