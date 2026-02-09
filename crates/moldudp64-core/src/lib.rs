04e4833a23348e2fdc51fcf9ed9562d6c4dafa6f
george-sx <stewgeorge3@gmail.com>
2026-02-08 20:29:05 -0500

    Create Structs for Header, MessageBlock, Packets, and SessionTable

--- crates/moldudp64-core/src/lib.rs
+++ crates/moldudp64-core/src/lib.rs
@@ -3,3 +3,29 @@ use std::collections::HashMap;
 pub struct MOLDUDP64 {
     pub socket: tokio::net::UdpSocket,
 }
+
+pub struct Header {
+    pub session_id: [u8; 10],
+    pub sequence_number: [u8; 8],
+    pub message_count: [u8; 2],
+}
+
+pub struct MessageBlock {
+    pub message_length: [u8; 2],
+    pub message_data: Vec<u8>,
+}
+
+pub struct Packet {
+    pub header: Header,
+    pub messages: Vec<MessageBlock>,
+}
+
+pub struct RequestPacket {
+    pub session_id: [u8; 10],
+    pub sequence_number: [u8; 8],
+    pub message_count: [u8; 2],
+}
+
+pub struct SessionTable {
+    pub sessions: HashMap<[u8; 10], [u8; 8]>,
+}
