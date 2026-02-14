191a13937745ee94fef5eba2686eb6f728544338
george-sx <stewgeorge3@gmail.com>
2026-02-14 14:59:37 -0500

    Revert client main.rs

--- crates/client/src/main.rs
+++ crates/client/src/main.rs
@@ -1,9 +1,3 @@
-mod moldudp64_client;
-use moldudp64_client::MoldConsumer;
-
-#[tokio::main]
-async fn main() -> std::io::Result<()> {
-    let mold = MoldConsumer::initialize("127.0.0.1:8081").await?;
-    mold.consume().await?;
-    Ok(())
+fn main() {
+    println!("Hello, world!");
 }
